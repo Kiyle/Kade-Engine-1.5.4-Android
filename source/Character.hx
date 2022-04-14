d1b7ead53cf0eaba034f958c868e5a0f2acfccb6
Kiyle <100969735+Kiyle@users.noreply.github.com>
2022-04-14 19:05:06 -0300

    Update Character Hx

--- source/Character.hx
+++ source/Character.hx
@@ -30,37 +30,94 @@ class Character extends FlxSprite
 
 		switch (curCharacter)
 		{
-			case 'gf':
-				// GIRLFRIEND CODE
-				tex = Paths.getSparrowAtlas('characters/GF_assets');
+			
+
+	
+
+				var tex = Paths.getSparrowAtlas('gf/GF_assets');
+
+				switch PlayState.curStage.toLowerCase() {
+
+					case 'spooky':
+
+						tex = jointex(tex, Paths.getSparrowAtlas('gf/GF_assets_week2'));
+
+					case 'stage':
+
+						if (PlayState.SONG.song.toLowerCase() == 'tutorial')
+
+						{
+
+							tex = jointex(tex, Paths.getSparrowAtlas('gf/GF_assets_tutorial'));
+
+						}
+
+					case 'philly':
+
+						tex = jointex(tex, jointex(Paths.getSparrowAtlas('gf/GF_assets_blowing'), Paths.getSparrowAtlas('gf/GF_ass_sets_landing')));
+
+				}
+
 				frames = tex;
+
 				animation.addByPrefix('cheer', 'GF Cheer', 24, false);
+
 				animation.addByPrefix('singLEFT', 'GF left note', 24, false);
+
 				animation.addByPrefix('singRIGHT', 'GF Right Note', 24, false);
+
 				animation.addByPrefix('singUP', 'GF Up Note', 24, false);
+
 				animation.addByPrefix('singDOWN', 'GF Down Note', 24, false);
+
 				animation.addByIndices('sad', 'gf sad', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], "", 24, false);
+
 				animation.addByIndices('danceLeft', 'GF Dancing Beat', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
+
 				animation.addByIndices('danceRight', 'GF Dancing Beat', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
+
 				animation.addByIndices('hairBlow', "GF Dancing Beat Hair blowing", [0, 1, 2, 3], "", 24);
+
 				animation.addByIndices('hairFall', "GF Dancing Beat Hair Landing", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], "", 24, false);
+
 				animation.addByPrefix('scared', 'GF FEAR', 24);
 
 				addOffset('cheer');
+
 				addOffset('sad', -2, -2);
+
 				addOffset('danceLeft', 0, -9);
+
 				addOffset('danceRight', 0, -9);
 
 				addOffset("singUP", 0, 4);
+
 				addOffset("singRIGHT", 0, -20);
+
 				addOffset("singLEFT", 0, -19);
+
 				addOffset("singDOWN", 0, -20);
+
 				addOffset('hairBlow', 45, -8);
+
 				addOffset('hairFall', 0, -9);
 
 				addOffset('scared', -2, -17);
 
 				playAnim('danceRight');
+	
+				
+				
+				
+				
+	
+				
+				
+				
+
+			
+
+				
 
 			case 'gf-christmas':
 				tex = Paths.getSparrowAtlas('characters/gfChristmas');
